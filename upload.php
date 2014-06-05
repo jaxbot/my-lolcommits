eacf7714b2728203003cf5ba33a0420d4f4825e7
Jonathan Warner <jaxbot@gmail.com>
2014-06-05 17:46:41 -0400

    Trim token

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 
-if ($_GET['token'] !== file_get_contents(".token"))
+if (trim($_GET['token']) != trim(file_get_contents(".token")))
 	die("Invalid token");
 
 file_put_contents("img/" . date("m_d_y_h_i_s_a") . ".png", $_POST['file']);
