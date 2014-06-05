7731777fd40c7ede27c519cad72653c66e9ffb30
Jonathan Warner <jaxbot@gmail.com>
2014-06-05 17:35:45 -0400

    Make actual HTML

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Jaxbot's Lolcommits</title>
+</head>
+<body>
+<h1>Jaxbot's Lolcommits</h1>
+
+These images are generated whenever I commit code, thanks to <a href="https://github.com/mroth/lolcommits">lolcommits</a>.<br><br>
+
 <?php
 
 $files = scandir("img/");
@@ -5,7 +15,12 @@ $files = scandir("img/");
 foreach ($files as $file) {
 	if (substr($file, 0, 1) == ".") continue;
 ?>
-	<img src="<?=$file?>" alt="lolcommit <?=$file?>"><br><br>
+	<img src="img/<?=$file?>" alt="lolcommit <?=$file?>"><br><br>
 <?php
 }
+
+?>
+
+</body>
+</html>
 
