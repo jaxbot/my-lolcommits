982de8d16c7a3c543048ffee2a71bbbb74e7e7be
Jonathan Warner <jaxbot@gmail.com>
2014-06-06 23:29:14 -0400

    Add unix time as a prefix

--- upload.php
+++ upload.php
@@ -3,5 +3,5 @@
 if (trim($_GET['token']) != trim(file_get_contents(".token")))
 	die("Invalid token");
 
-file_put_contents("img/" . date("m_d_y_h_i_s_a") . ".png", base64_decode($_POST['file']));
+file_put_contents("img/" . time() . "_" . date("m_d_y_h_i_s_a") . ".png", base64_decode($_POST['file']));
 
